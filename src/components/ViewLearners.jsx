e72b0d17d5fa0fa1614ab2bb92a8c1cf68eeb519
Martin Otieno <monyango21@student.kibu.ac.ke>
2026-03-02 13:53:20 -0600

    fetching data using db.json

--- src/components/ViewLearners.jsx
+++ src/components/ViewLearners.jsx
@@ -4,18 +4,34 @@ import { useEffect, useState } from "react"
 export default function ViewLearners(){
 
     const [name, setName] = useState("")
+    const [posts, setPosts] =useState([])
 
     // syntax of use effect
      // useEffect(function, dependency)
-     useEffect (()=>
-        console.log("Kelvin"), [name]
-    )
+     useEffect (()=>{
+        fetch("https://jsonplaceholder.typicode.com/posts")
+        .then( res=>res.json() )
+        .then( (data)=>{
 
+            setPosts(data)
+            console.log(data)
+        } )
+     }, [])
+
+     console.log("post", "posts")
 
     return (
         <div>
             <h2 className="font-bold text-2xl rounded-lg">SUDENTS: {name}</h2>
             <input onChange={(e)=>setName(e.target.value)} className="border" type="text" />
+            {
+                posts.map((post, index)=>(
+                    <div key={index}>
+                        <h1>{post.title}</h1>
+                        <p>{post.body}</p>
+                    </div>
+                ))
+            }
         </div>
     );
 }
